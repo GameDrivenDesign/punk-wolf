78cd5a987e1cf5899ad9c594aa509cc708e91612
Tom Beckmann <tomjonabc@gmail.com>
2020-09-27 10:27:50 +0200

    improve newgame slide

--- newgame.gd
+++ newgame.gd
@@ -3,3 +3,24 @@ extends Node2D
 func _input(event):
 	if event is InputEventKey:
 		get_tree().change_scene("res://game.tscn")
+
+func _ready():
+	get_current_highscore()
+
+func get_current_highscore():
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	
+	var url = ("https://scores.tmbe.me/score?game=" + Global.GAME_NAME.percent_encode())
+	
+	if http_request.request(url, [], true, HTTPClient.METHOD_GET) != OK:
+		print("Loading highscore failed")
+		return -1
+	var response = yield(http_request, "request_completed")
+	if response[1] != 200:
+		print("Loading highscore returned " + str(response[1]))
+		return -1
+	remove_child(http_request)
+	
+	var data = JSON.parse(response[3].get_string_from_utf8()).result[0]
+	$Current.text = "Current Highscore:\n    " + data["player"] + "    " + str(data["score"])
