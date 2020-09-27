08ea3869e82da6343decaf392d3e6b3eae187740
Tom Beckmann <tomjonabc@gmail.com>
2020-09-27 12:31:38 +0200

    add config option for four colors

--- global.gd
+++ global.gd
@@ -5,11 +5,23 @@ const PLAYER_PADDING_HORIZONTAL: Vector2 = Vector2(400, 480)
 const PADDING_TOP: float = 80.0
 const GAME_NAME = "Punk Wolf"
 
+const w = Color(3, 3, 3)
 const b = Color(1, 1, 7)
 const r = Color(4, 1, 1)
 const rb = Color(3.5, 1, 3.5)
-const rrb = Color(4, 1, 2)
-const rbb = Color(2.5, 1, 4.5)
-const PROJECTILE_COLORS = [r, b, rb, rrb, rbb]
+
+const useFourColors = false
+
+const _rrb = Color(4, 1, 2)
+const _rbb = Color(2.5, 1, 4.5)
+var rbb = _rbb setget ,get_rbb
+var rrb = _rrb setget ,get_rrb
+
+var PROJECTILE_COLORS = [r, b, rb, rrb, rbb]
+
+func get_rbb():
+	return w if useFourColors else _rbb
+func get_rrb():
+	return w if useFourColors else _rrb
 
 var highscore = 0
--- sheep/shield.gd
+++ sheep/shield.gd
@@ -4,7 +4,7 @@ const DECAY = 8
 const SHIELD_COLOR_TOLERANCE = 0.3
 var r_index = 0
 var b_index = 0
-var colors = [1, 2.5, 4]
+var colors = [1, 4] if Global.useFourColors else [1, 2.5, 4]
 
 # warning-ignore:unused_signal
 signal shield_changed
@@ -14,18 +14,27 @@ func is_blocked(projectile):
 	var color = get_modulate()
 	var shield_vector = Vector3(color.r - 1, color.g - 1, color.b - 1).normalized()
 	var projectile_vector = Vector3(projectile_color.r - 1, projectile_color.g - 1, projectile_color.b - 1).normalized()
+	if (Global.useFourColors &&
+		projectile_vector.x == projectile_vector.z && projectile_vector.y == projectile_vector.z &&
+		shield_vector.x == shield_vector.y && shield_vector.x == shield_vector.z):
+		return true
 	return projectile_vector.distance_to(shield_vector) < SHIELD_COLOR_TOLERANCE
 
 func _process(_delta):
-	if Input.is_action_just_pressed("shield_red"):
-		r_index = (r_index + 1) % len(colors)
+	if Global.useFourColors:
+		r_index = 1 if Input.is_action_pressed("shield_red") else 0
+		b_index = 1 if Input.is_action_pressed("shield_blue") else 0
 		$"../..".emit_signal("shield_changed", true, r_index)
-	
-	if Input.is_action_just_pressed("shield_blue"):
-		b_index = (b_index + 1) % len(colors)
 		$"../..".emit_signal("shield_changed", false, b_index)
+	else:
+		if Input.is_action_just_pressed("shield_red"):
+			r_index = (r_index + 1) % len(colors)
+			$"../..".emit_signal("shield_changed", true, r_index)
+		if Input.is_action_just_pressed("shield_blue"):
+			b_index = (b_index + 1) % len(colors)
+			$"../..".emit_signal("shield_changed", false, b_index)
 	
 	var r = colors[r_index]
 	var b = colors[b_index]
 	
-	set_modulate(Color(r, 1, b, min(1, r - 1 + b - 1)))
+	set_modulate(Color(r, 1, b, 1 if Global.useFourColors else min(1, r - 1 + b - 1)))
